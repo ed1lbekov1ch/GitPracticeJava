2c659f22800a9a304cc96c39d213787778fddc72
Aibek Abdyrazakov <aibekedilbekovich@gmail.com>
2023-08-29 19:23:00 +0600

    First commit

---
+++ src/main/java/com/example/javaspringgitpractice_1/JavaSpringGitPractice1Application.java
@@ -0,0 +1,10 @@
+package com.example.javaspringgitpractice_1;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringGitPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringGitPractice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringgitpractice_1/JavaSpringGitPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringgitpractice_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringGitPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
